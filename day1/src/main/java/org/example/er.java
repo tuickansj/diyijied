0715aa2f098ea802444010b93f305472ca376017
tuickansj <1984183978@qq.com>
2023-09-26 10:19:06 +0800

    更改

--- day1/src/main/java/org/example/er.java
+++ day1/src/main/java/org/example/er.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class er {
     public static void main(String[] args) {
-        System.out.println("ssssaaaaaaaa");
+        System.out.println("ssssaaaaaasssssaa");
     }
 }
